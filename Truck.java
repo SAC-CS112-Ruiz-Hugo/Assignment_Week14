84e3efe2c7bae753a00f4dac56bcd33af2f93594
SAC-CS112-Ruiz-Hugo <HjRuiz93@yahoo.com>
2016-11-26 18:42:46 -0800

    Add files via upload

---
+++ Truck.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Cule on 11/26/2016.
+ */
+public class Truck extends Automobile
+{
+    public Truck(int year, String make, String model, String color, String name, int numOfGears, int operatingCycle) {
+        super();
+
+    }
+
+}
---
+++ Van.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Cule on 11/26/2016.
+ */
+public class Van extends Automobile
+{
+    public Van(int year, String make, String model, String color, String name, int numOfGears, int operatingCycle) {
+        super();
+
+    }
+
+}
